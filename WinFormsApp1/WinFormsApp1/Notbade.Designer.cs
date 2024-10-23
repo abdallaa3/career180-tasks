e5201bb4a6af121054d7e88e179e49e2f5c9793e
Abdallah Kmar <106463323+abdallaa3@users.noreply.github.com>
2024-10-23 17:59:48 +0300

    Add files via upload

---
+++ WinFormsApp1/WinFormsApp1/Notbade.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace WinFormsApp1
+{
+    partial class Notbade
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            menuStrip1 = new MenuStrip();
+            fileToolStripMenuItem = new ToolStripMenuItem();
+            newToolStripMenuItem = new ToolStripMenuItem();
+            openToolStripMenuItem = new ToolStripMenuItem();
+            saveToolStripMenuItem = new ToolStripMenuItem();
+            saveAsToolStripMenuItem = new ToolStripMenuItem();
+            closeToolStripMenuItem = new ToolStripMenuItem();
+            editToolStripMenuItem = new ToolStripMenuItem();
+            fontToolStripMenuItem = new ToolStripMenuItem();
+            colorToolStripMenuItem = new ToolStripMenuItem();
+            backColorToolStripMenuItem = new ToolStripMenuItem();
+            aboutToolStripMenuItem = new ToolStripMenuItem();
+            rtb_body = new RichTextBox();
+            saveFD = new SaveFileDialog();
+            openFD = new OpenFileDialog();
+            fontD = new FontDialog();
+            colorD = new ColorDialog();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.ImageScalingSize = new Size(20, 20);
+            menuStrip1.Items.AddRange(new ToolStripItem[] { fileToolStripMenuItem, editToolStripMenuItem, aboutToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(800, 31);
+            menuStrip1.TabIndex = 0;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            fileToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { newToolStripMenuItem, openToolStripMenuItem, saveToolStripMenuItem, saveAsToolStripMenuItem, closeToolStripMenuItem });
+            fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            fileToolStripMenuItem.Size = new Size(49, 27);
+            fileToolStripMenuItem.Text = "File";
+            // 
+            // newToolStripMenuItem
+            // 
+            newToolStripMenuItem.Name = "newToolStripMenuItem";
+            newToolStripMenuItem.Size = new Size(152, 28);
+            newToolStripMenuItem.Text = "New";
+            newToolStripMenuItem.Click += newToolStripMenuItem_Click;
+            // 
+            // openToolStripMenuItem
+            // 
+            openToolStripMenuItem.Name = "openToolStripMenuItem";
+            openToolStripMenuItem.Size = new Size(152, 28);
+            openToolStripMenuItem.Text = "open";
+            openToolStripMenuItem.Click += openToolStripMenuItem_Click;
+            // 
+            // saveToolStripMenuItem
+            // 
+            saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            saveToolStripMenuItem.Size = new Size(152, 28);
+            saveToolStripMenuItem.Text = "Save";
+            saveToolStripMenuItem.Click += saveToolStripMenuItem_Click;
+            // 
+            // saveAsToolStripMenuItem
+            // 
+            saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+            saveAsToolStripMenuItem.Size = new Size(152, 28);
+            saveAsToolStripMenuItem.Text = "Save As";
+            saveAsToolStripMenuItem.Click += saveAsToolStripMenuItem_Click;
+            // 
+            // closeToolStripMenuItem
+            // 
+            closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            closeToolStripMenuItem.Size = new Size(152, 28);
+            closeToolStripMenuItem.Text = "Close";
+            closeToolStripMenuItem.Click += closeToolStripMenuItem_Click;
+            // 
+            // editToolStripMenuItem
+            // 
+            editToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { fontToolStripMenuItem, colorToolStripMenuItem, backColorToolStripMenuItem });
+            editToolStripMenuItem.Name = "editToolStripMenuItem";
+            editToolStripMenuItem.Size = new Size(53, 27);
+            editToolStripMenuItem.Text = "Edit";
+            // 
+            // fontToolStripMenuItem
+            // 
+            fontToolStripMenuItem.Name = "fontToolStripMenuItem";
+            fontToolStripMenuItem.Size = new Size(224, 28);
+            fontToolStripMenuItem.Text = "Font";
+            fontToolStripMenuItem.Click += fontToolStripMenuItem_Click;
+            // 
+            // colorToolStripMenuItem
+            // 
+            colorToolStripMenuItem.Name = "colorToolStripMenuItem";
+            colorToolStripMenuItem.Size = new Size(224, 28);
+            colorToolStripMenuItem.Text = "Color";
+            colorToolStripMenuItem.Click += colorToolStripMenuItem_Click;
+            // 
+            // backColorToolStripMenuItem
+            // 
+            backColorToolStripMenuItem.Name = "backColorToolStripMenuItem";
+            backColorToolStripMenuItem.Size = new Size(224, 28);
+            backColorToolStripMenuItem.Text = "Back Color";
+            backColorToolStripMenuItem.Click += backColorToolStripMenuItem_Click;
+            // 
+            // aboutToolStripMenuItem
+            // 
+            aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            aboutToolStripMenuItem.Size = new Size(69, 27);
+            aboutToolStripMenuItem.Text = "about";
+            // 
+            // rtb_body
+            // 
+            rtb_body.Dock = DockStyle.Fill;
+            rtb_body.Location = new Point(0, 31);
+            rtb_body.Name = "rtb_body";
+            rtb_body.Size = new Size(800, 419);
+            rtb_body.TabIndex = 1;
+            rtb_body.Text = "";
+            // 
+            // openFD
+            // 
+            openFD.FileName = "openFileDialog1";
+            // 
+            // Notbade
+            // 
+            AutoScaleDimensions = new SizeF(9F, 23F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(rtb_body);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Name = "Notbade";
+            Text = "Notbade";
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem fileToolStripMenuItem;
+        private ToolStripMenuItem newToolStripMenuItem;
+        private ToolStripMenuItem openToolStripMenuItem;
+        private ToolStripMenuItem saveToolStripMenuItem;
+        private ToolStripMenuItem saveAsToolStripMenuItem;
+        private ToolStripMenuItem closeToolStripMenuItem;
+        private ToolStripMenuItem editToolStripMenuItem;
+        private ToolStripMenuItem fontToolStripMenuItem;
+        private ToolStripMenuItem colorToolStripMenuItem;
+        private ToolStripMenuItem backColorToolStripMenuItem;
+        private ToolStripMenuItem aboutToolStripMenuItem;
+        private RichTextBox rtb_body;
+        private SaveFileDialog saveFD;
+        private OpenFileDialog openFD;
+        private FontDialog fontD;
+        private ColorDialog colorD;
+    }
+}
---
+++ WinFormsApp1/WinFormsApp1/Notbade.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp1
+{
+    public partial class Notbade : Form
+    {
+        string path;
+        bool isTextChanged = false; 
+        public Notbade()
+        {
+            InitializeComponent();
+            path = null;
+            UpdateTitle();
+        }
+
+        private void UpdateTitle()
+        {
+            if (string.IsNullOrEmpty(path))
+                this.Text = "Untitled - Notbade";
+            else
+                this.Text = System.IO.Path.GetFileName(path) + " - Notbade";
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(path))
+            {
+                rtb_body.SaveFile(path);
+                ClearBackColor(); 
+                rtb_body.Clear();
+                isTextChanged = false; 
+            }
+            else
+            {
+                if (saveFD.ShowDialog() == DialogResult.OK)
+                {
+                    rtb_body.SaveFile(saveFD.FileName);
+                    path = saveFD.FileName;
+                    ClearBackColor(); 
+                    rtb_body.Clear();
+                    isTextChanged = false; 
+                    UpdateTitle();
+                }
+            }
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (isTextChanged && ConfirmSaveChanges()) 
+            {
+                saveToolStripMenuItem_Click(null, null);
+            }
+
+            if (openFD.ShowDialog() == DialogResult.OK)
+            {
+                rtb_body.LoadFile(openFD.FileName);
+                path = openFD.FileName;
+                isTextChanged = false; 
+                UpdateTitle();
+            }
+        }
+
+        private void saveAsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (saveFD.ShowDialog() == DialogResult.OK)
+            {
+                rtb_body.SaveFile(saveFD.FileName);
+                path = saveFD.FileName;
+                ClearBackColor(); 
+                rtb_body.Clear();
+                isTextChanged = false;   
+                UpdateTitle();
+            }
+        }
+
+        private void newToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (isTextChanged && ConfirmSaveChanges()) 
+            {
+                saveToolStripMenuItem_Click(null, null);
+            }
+
+            rtb_body.Clear();
+            path = null;
+            isTextChanged = false; 
+            UpdateTitle();
+        }
+
+        private void closeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (isTextChanged && ConfirmSaveChanges()) 
+            {
+                saveToolStripMenuItem_Click(null, null);
+            }
+
+            this.Close();
+        }
+
+        private bool ConfirmSaveChanges()
+        {
+            return MessageBox.Show("You have unsaved changes. Do you want to save them?", "Unsaved Changes", MessageBoxButtons.YesNo) == DialogResult.Yes;
+        }
+
+        private void rtb_body_TextChanged(object sender, EventArgs e)
+        {
+            isTextChanged = true;
+        }
+
+        private void fontToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (fontD.ShowDialog() == DialogResult.OK)
+            {
+                if (rtb_body.SelectedText.Length > 0)
+                {
+                    rtb_body.SelectionFont = fontD.Font;
+                }
+                else
+                {
+                    rtb_body.Font = fontD.Font;
+                }
+            }
+        }
+
+        private void colorToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (colorD.ShowDialog() == DialogResult.OK)
+            {
+                if (rtb_body.SelectedText.Length > 0)
+                {
+                    rtb_body.SelectionColor = colorD.Color;
+                }
+                else
+                {
+                    rtb_body.ForeColor = colorD.Color;
+                }
+            }
+        }
+
+        private void backColorToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (colorD.ShowDialog() == DialogResult.OK)
+            {
+                if (rtb_body.SelectedText.Length > 0)
+                {
+                    rtb_body.SelectionBackColor = colorD.Color;
+                }
+                else
+                {
+                    rtb_body.BackColor = colorD.Color;
+                }
+            }
+        }
+
+        private void ClearBackColor()
+        {
+            rtb_body.BackColor = Color.White; 
+        }
+
+        private void undoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (rtb_body.CanUndo)
+            {
+                rtb_body.Undo();
+            }
+        }
+
+        private void redoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (rtb_body.CanRedo)
+            {
+                rtb_body.Redo();
+            }
+        }
+    }
+}
