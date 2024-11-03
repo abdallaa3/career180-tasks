9ec14cca4c9e6597dcaa7165fcf9be96c6d213f7
abdallaa3 <abdallaamoe@gmail.com>
2024-11-03 21:05:47 +0200

    API DAY1 C180 Assi

---
+++ Day1/Controllers/coursesController.cs
@@ -0,0 +1,33 @@
+﻿using Api_c180_Day1.models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api_c180_Day1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class coursesController : ControllerBase
+    {
+        public List<course> courses = new List<course>
+        {
+        new course { Id = 1, Name = "C++", Duration = 10, Status = true },
+        new course { Id = 2, Name = "C#", Duration = 8, Status = false },
+        new course { Id = 3, Name = "Sql", Duration = 12, Status = true }
+        };
+
+        [HttpGet]
+        public List<course> GetAll()
+        {
+            return courses;
+        }
+
+        [HttpGet("{id}")]
+        public course GetById(int id)
+        {
+            return courses.FirstOrDefault(c => c.Id == id);
+        }
+
+
+    }
+}
+
---
+++ Day1/models/course.cs
@@ -0,0 +1,10 @@
+﻿namespace Api_c180_Day1.models
+{
+    public class course
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Duration { get; set; }
+        public bool Status { get; set; }
+    }
+}
